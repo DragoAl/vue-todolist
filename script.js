803901d8462c3c66fc2fcf437f3ed27b8240477e
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-11-12 14:24:22 +0100

    print items of array

---
+++ script.js
@@ -0,0 +1,30 @@
+var app = new Vue ({
+    el: '#todo',
+    data : {
+        items : [
+            {
+                text: 'fare la spesa',
+                done : false
+            },
+            {
+                text: 'portare il gatto dal veterinario',
+                done : false
+            },
+            {
+                text: 'pulire il bagno',
+                done : false
+            } ,   
+            {
+                text: 'allenarsi',
+                done : false
+            } ,   
+            {
+                text: 'scrivere codice',
+                done : false
+            },    
+          
+        ],
+    }
+}
+
+)
